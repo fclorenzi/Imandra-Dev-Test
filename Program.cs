158dc2e6a9517d989b07a61ad6b57a35d7ced209
Franco Lorenzi <fclorenzi@gmail.com>
2024-07-28 18:03:04 -0500

    Added structure for parameters
    Adding display help
    Shows display help when parameters are not correct

--- Program.cs
+++ Program.cs
@@ -1,10 +1,94 @@
-﻿namespace Imandra_Dev_Test;
+﻿using System.Text;
+
+namespace Imandra_Dev_Test;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //Help display text
+        StringBuilder strHelp = new StringBuilder();
+        strHelp.AppendLine("Please enter valid argumenst.");
+        strHelp.AppendLine("-For list of stransactions use 'print-messages fix-file-with-path tags-file-with-path'");
+        strHelp.AppendLine("-For Part 2 Question 1 use 'part2-q1 fix-file-with-path'");
+        strHelp.AppendLine("-For Part 2 Question 1 use 'part2-q2 fix-file-with-path'");
+        strHelp.AppendLine("-For Part 2 Question 1 use 'part2-q3 fix-file-with-path'");
+        strHelp.AppendLine("-For Part 3 Question 1 use 'part3-q1 fix-file-with-path'");
+
+        if (args.Length == 0)
+        {
+            //If no arguments given return help
+            Console.Write(strHelp);
+        }
+        else
+        {
+            switch (args[0])
+            {
+                case "print-messages":
+                    if (args.Length > 2)
+                    {
+                        Console.Write("print-messages");
+                    }
+                    else
+                    {
+                        //if not enough arguments given return help
+                        Console.Write(strHelp);
+                    }
+                    break;
+                case "part2-q1":
+                    if (args.Length > 1)
+                    {
+                        Console.WriteLine("part2-q1");
+                    }
+                    else
+                    {
+                        //if not enough arguments given return help
+                        Console.Write(strHelp);
+                    }
+                    break;
+                case "part2-q2":
+                    if (args.Length > 1)
+                    {
+                        Console.WriteLine("part2-q2");
+                    }
+                    else
+                    {
+                        //if not enough arguments given return help
+                        Console.Write(strHelp);
+                    }
+                    break;
+                case "part2-q3":
+                    if (args.Length > 1)
+                    {
+                        Console.WriteLine("part2-q3");
+                    }
+                    else
+                    {
+                        //if not enough arguments given return help
+                        Console.Write(strHelp);
+                    }
+                    break;
+                case "part3-q1":
+                    if (args.Length > 1)
+                    {
+                        Console.WriteLine("part3-q1");
+                    }
+                    else
+                    {
+                        //if not enough arguments given return help
+                        Console.Write(strHelp);
+                    }
+                    break;
+                default:
+                    //if no valid action given return help
+                    Console.Write(strHelp);
+                    break;
+            }
+        }
+
+
+
     }
 }
 

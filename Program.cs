de59a9b8485d2b871829fab3475356b0d5b8a2a3
Franco Lorenzi <fclorenzi@gmail.com>
2024-07-28 18:27:47 -0500

    Part 1 - Parsing load files
    Going over the text transaction lines and breaking down into fields.
    Look up to tags file to set up the field name, data type and if needed to map enums

--- Program.cs
+++ Program.cs
@@ -6,6 +6,74 @@ namespace Imandra_Dev_Test;
 class Program
 {
 
+    //Gets the name of the tag from json
+    static string getTagName(JObject json, string itemId)
+    {
+        return json[itemId]["name"].ToString();
+    }
+
+    //Gets the data type of the tag from json
+    static string getTagType(JObject json, string itemId)
+    {
+        return json[itemId]["type"].ToString();
+    }
+
+    //Gets the enum text value of the tag from json
+    static string getTagEnumValue(JObject json, string itemId, string valueId)
+    {
+        return json[itemId]["values"][valueId].ToString();
+    }
+
+    //Converts list of string transactions to object lists for JSON formatting
+    static List<object> getTransactionListFromText(List<string> transactions, JObject jsonTags)
+    {
+        List<object> retVal = new List<object>();
+
+        //Iterating the list of transactions
+        foreach (string trans in transactions)
+        {
+            //Define a new transaction as a list of fields
+            List<KeyValuePair<string, object>> lisNewTransaction = new List<KeyValuePair<string, object>>();
+
+            //Iterating the fields on each transaction
+            foreach (string field in trans.Split("|"))
+            {
+                //Getting field label
+                string fieldNumber = field.Split("=")[0];
+                //Getting field value
+                string fieldValue = field.Split("=")[1];
+
+                KeyValuePair<string, object> dictField = new KeyValuePair<string, object>();
+
+                string finalName = getTagName(jsonTags, fieldNumber);
+                //Evaluating field data type and additional enum values
+                switch (getTagType(jsonTags, fieldNumber))
+                {
+                    case "enum":
+                        fieldValue = getTagEnumValue(jsonTags, fieldNumber, fieldValue);
+                        dictField = new KeyValuePair<string, object>(finalName, fieldValue);
+                        break;
+                    case "int":
+                        dictField = new KeyValuePair<string, object>(finalName, int.Parse(fieldValue));
+                        break;
+                    case "decimal":
+                        dictField = new KeyValuePair<string, object>(finalName, decimal.Parse(fieldValue));
+                        break;
+                    default:
+                        dictField = new KeyValuePair<string, object>(finalName, fieldValue);
+                        break;
+                }
+                //Adding field to transaction
+                lisNewTransaction.Add(dictField);
+            }
+            //Converting KeyValuePair transaction fields to Dictionary for JSON ease of serialization
+            var dictionary = lisNewTransaction.ToDictionary(x => x.Key, x => x.Value);
+            //Add transaction to final list
+            retVal.Add(dictionary);
+        }
+        return retVal;
+    }
+
     static string getJsonMessages(string[] args, string ProtocolHeader)
     {
         string retVal = "";
@@ -22,7 +90,15 @@ class Program
         StreamReader sr = new StreamReader(fixFileName);
         string line = sr.ReadLine();
 
-        retVal = line;
+        //Splitting fix file into a list of text transactions
+        //Using the header text and removing the first item because it is empty
+        List<string> lisTransactions = (from x in line.Split(ProtocolHeader).Skip(1).ToList() select ProtocolHeader + x.Remove(x.Length - 1)).ToList();
+
+        //Convert list of text transactions to list of objects for easy JSON serialization
+        List<object> lisFinalTransactions = getTransactionListFromText(lisTransactions, jsonTags);
+
+        //Serializing and displaying list of transactions
+        retVal = System.Text.Json.JsonSerializer.Serialize(lisFinalTransactions);
 
         return retVal;
     }
